c1a0d34ba3ed84df1a0b2b4365be4e08e88f4fa2
tbastasch <tbastasch@zagmail.gonzaga.edu>
2016-07-02 23:18:43 -0700

    need to finish: footer touches

--- imports/components/public/footer.jsx
+++ imports/components/public/footer.jsx
@@ -1,25 +1,31 @@
 import React from 'react';
 import FontAwesome from 'react-fontawesome';
-import SnapModal from './snap_modal.jsx'
+import Headroom from 'react-headroom';
+
+import SnapModal from './snap_modal.jsx';
 
 export default class Footer extends React.Component {
   render() {
     return(
-      <div className="row footer x-center center">
-        <a href="https://www.twitch.tv/brachyon" target="_blank"><FontAwesome name="twitch" className="twitch icon" /></a>
-        <span className="footer-div">.</span>
-        <a href="https://www.youtube.com/channel/UCUrPEefFomt33g048J3nmcg" target="_blank"><FontAwesome name="youtube-square" className="youtube icon" /></a>
-        <span className="footer-div">.</span>
-        <a href="https://www.reddit.com/r/brachyon" target="_blank"><FontAwesome name="reddit-square" className="reddit icon" /></a>
-        <span className="footer-div">.</span>
-        <FontAwesome name="facebook-square " className="facebook icon" />
-        <span className="footer-div">.</span>
-        <SnapModal />
-        <span className="footer-div">.</span>
-        <a href="https://twitter.com/brachyon" target="_blank"><FontAwesome name="twitter-square" className="twitter icon" /></a>
-        <span className="footer-div">.</span>
-        <a href="https://www.instagram.com/brachyon/" target="_blank"><FontAwesome name="instagram" className="instagram icon" /></a>
-      </div>
+      <Headroom
+        className="footer-room"
+      >
+        <div className="row footer x-center center">
+          <a href="https://www.twitch.tv/brachyon" target="_blank"><FontAwesome name="twitch" className="twitch" /></a>
+          <span className="footer-div">.</span>
+          <a href="https://www.youtube.com/channel/UCUrPEefFomt33g048J3nmcg" target="_blank"><FontAwesome name="youtube-square" className="youtube icon" /></a>
+          <span className="footer-div">.</span>
+          <a href="https://www.reddit.com/r/brachyon" target="_blank"><FontAwesome name="reddit-square" className="reddit icon" /></a>
+          <span className="footer-div">.</span>
+          <FontAwesome name="facebook-square " className="facebook icon" />
+          <span className="footer-div">.</span>
+          <SnapModal />
+          <span className="footer-div">.</span>
+          <a href="https://twitter.com/brachyon" target="_blank"><FontAwesome name="twitter-square" className="twitter icon" /></a>
+          <span className="footer-div">.</span>
+          <a href="https://www.instagram.com/brachyon/" target="_blank"><FontAwesome name="instagram" className="instagram icon" /></a>
+        </div>
+      </Headroom>
     );
   }
 }
