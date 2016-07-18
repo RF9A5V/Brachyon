0e266ffcdb09beb50c3776732e7946e0dcdc432d
Steven Chen <steven@thecrudeintellectual.com>
2016-07-17 20:07:44 -0700

    Smarter contributed vs amount to contribute UI for the slider

--- imports/components/public/slider.jsx
+++ imports/components/public/slider.jsx
@@ -26,18 +26,20 @@ export default class PaymentSlider extends Component {
 
   componentWillReceiveProps(next) {
     var max = this.props.tiers[Object.keys(this.props.tiers).length - 1].price * 1;
+    this.refs.amount.value = "";
     this.setState({
-      sliderPosition: `${Math.min(next.contrib / max * 100, 100)}%`
+      sliderPosition: `${Math.min(next.contrib / max * 100, 100)}%`,
+      amount: 0
     })
   }
 
   tierSelect(key, left, amount) {
     return function(e) {
-      this.refs.amount.value = amount;
+      this.refs.amount.value = amount - this.props.contrib;
       this.setState({
         sliderPosition: left + "%",
         key,
-        amount
+        amount: amount - this.props.contrib
       });
     }
   }
@@ -48,11 +50,19 @@ export default class PaymentSlider extends Component {
     var max = this.props.tiers[keys[keys.length - 1]].price * 1;
     return keys.map((key) => {
       var left = self.props.tiers[key].price * 1 / max * 100;
+      var price = self.props.tiers[key].price * 1;
+      var buyable = price > this.props.contrib;
       return (
-        <div className="col slider-tier" style={{left: `${left}%`}} onClick={self.tierSelect(key, left, self.props.tiers[key].price * 1).bind(this)}>
+        <div className="col slider-tier noselect" style={{left: `${left}%`}} onClick={
+          buyable ? (
+            self.tierSelect(key, left, self.props.tiers[key].price * 1).bind(this)
+          ) : (
+            () => {}
+          )
+        }>
           <div style={{width: 5, height: 20, backgroundColor: "white"}}></div>
-          <span>
-            { self.props.tiers[key].price }
+          <span style={{color: buyable ? ("white") : ("#888")}}>
+            { price }
           </span>
         </div>
       )
@@ -67,7 +77,7 @@ export default class PaymentSlider extends Component {
         toastr.error(err.reason, "Error!");
       }
       else {
-        toastr.success("Bueno!", "Success!");
+        toastr.success(`${this.state.amount} successfully sponsored for this event!`, "Success!");
         self.refs.amount.value = 0;
         self.setState({
           amount: 0
@@ -103,10 +113,38 @@ export default class PaymentSlider extends Component {
     })
   }
 
+  sliderDragStart(e) {
+    this.setState({
+      dragging: true
+    })
+  }
+
+  sliderDragMove(e) {
+    e.preventDefault();
+    if(!this.state.dragging){
+      return;
+    }
+    var target = e.target;
+    while(!target.classList.contains("slider-control")){
+      target = target.parentNode;
+    }
+  }
+
+  sliderDragEnd(e) {
+    this.setState({
+      dragging: false
+    })
+  }
+
   render() {
     return (
       <div style={{position: "relative", padding: "0 40px 0 20px"}}>
-        <div className="col slider-control" ref="sliderControl" style={{left: this.state.sliderPosition}}>
+        <div className="col slider-control noselect" ref="sliderControl" style={{left: this.state.sliderPosition}}
+        onMouseDown={this.sliderDragStart.bind(this)}
+        onMouseUp={this.sliderDragEnd.bind(this)}
+        onMouseMove={this.sliderDragMove.bind(this)}
+        onMouseOut={this.sliderDragEnd.bind(this)}
+        >
           <div className="slider-control-header">
             { (this.props.contrib * 1) + this.state.amount }
           </div>
@@ -122,9 +160,6 @@ export default class PaymentSlider extends Component {
             </div>
             <input style={{paddingLeft: 39, margin: 0, marginBottom: 10}} type="text" ref="amount" onChange={this.moveSlider.bind(this)}/>
           </div>
-          <button onClick={this.sponsorEvent.bind(this)}>
-            Sponsor!
-          </button>
         </div>
         {
           this.state.key !== null && this.props.tiers[this.state.key] ? (
@@ -134,7 +169,27 @@ export default class PaymentSlider extends Component {
                 <div>{this.props.tiers[this.state.key].limit - (this.props.tiers[this.state.key].count || 0)} left of {this.props.tiers[this.state.key].limit}</div>
               </div>
               <span>{ this.props.tiers[this.state.key].description }</span>
+              <div className="row center" style={{marginTop: 10}}>
+                {
+                  this.props.contrib >= this.props.tiers[this.state.key].price * 1 ? (
+                    <span>You have already purchased this tier!</span>
+                  ) : (
+                    ""
+                  )
+                }
+              </div>
             </div>
+          ) : (
+            ""
+          )
+        }
+        {
+          this.refs.amount && this.refs.amount.value * 1 > 0 ? (
+            <button className="row" onClick={this.sponsorEvent.bind(this)}>
+              <span>
+                Sponsor!
+              </span>
+            </button>
           ) : (
             ""
           )
