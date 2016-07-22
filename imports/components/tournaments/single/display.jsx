e1b6635d09c104a9e86154e47e1518c9e912dafc
axman36 <alex@renefe.com>
2016-07-22 16:33:27 -0700

    Finalization of single advancing

--- imports/components/tournaments/single/display.jsx
+++ imports/components/tournaments/single/display.jsx
@@ -30,7 +30,7 @@ export default class SingleDisplay extends Component {
   }
 
   formbrackets() {
-    var num = 3, matchn = 1, i, spacing = 0, m=0, n=0;
+    var num = 10, matchn = 1, i, spacing = 0, m=0, n=0;
     var nonbyes = (num - Math.pow(2, Math.floor(Math.log2(num))))*2;
     var byes = num - nonbyes;
     var rounds = Math.ceil(Math.log2(num));
@@ -41,29 +41,33 @@ export default class SingleDisplay extends Component {
     if (nonbyes > 0)
     {
       var nbarr = Array.apply(null, Array(roundparticipants*2)).map(function (_, i) {return i+1;});
-      var aseed = this.seed(nbarr), w = 0;
+      var aseed = this.seed(nbarr);
+      spot = [];
       matchn++;
       for (i = 0; i < nonbyes; i++)
+        spot.push(aseed.indexOf(byes+i+1));
+      spot = spot.sort(function(a, b){return a-b});
+      console.log(spot);
+      for (i = 0; i < nonbyes; i++) //byes+i+1 is how we access the nonbye numbers.
       {
-        var spot;
-        spot = aseed.indexOf(byes+i+1);
-        var boxid = "match" + spot + "nonbye";
+        var boxid = "match" + spot[i] + "nonbye";
         var style = {
-          top: spot*50 - 25 + "px",
+          top: spot[i]*50 - 25 + "px",
           left: 200 + "px",
           color: "white"
         }
         boxes.push(
-          <MatchBlock sty={style} eid={boxid} pos={-1} sp={spot} sp2 = {spot} key={n} ref={boxid} changematches={this.changematches.bind(this)}/>
+          <MatchBlock sty={style} eid={boxid} pos={-1} sp={byes+1+i} sp2 = {spot[i]} key={n} ref={boxid} changematches={this.changematches.bind(this)}/>
         )
         n++;
-        usedspots[Math.floor(i/2)] = Math.floor((spot)/2);
+        usedspots[Math.floor(i/2)] = Math.floor((spot[i])/2);
       }
     }
 
     //This is primary function for the style and the actual placement of the byes.
     while (matchn <= rounds+1)
     {
+      var w = 1;
       for (i = 0; i < roundparticipants; i++)
       {
           var boxid = "match" + i + "round" + matchn;
@@ -72,13 +76,12 @@ export default class SingleDisplay extends Component {
             left: matchn*200 + "px",
             color: "white"
           };
-          if (i%2 == 0 && usedspots.includes(i+1) || i%2 == 1 && usedspots.includes(i-1))
-            style.color = "gray";
           if (roundparticipants == (num - nonbyes/2) && (i == 0 || !(usedspots.includes(i))))
           {
             boxes.push(
-              <MatchBlock sty={style} eid={boxid} pos={matchn} sp={i+1} sp2 = {i} ref={boxid} changematches={this.changematches.bind(this)}/>
+              <MatchBlock sty={style} eid={boxid} pos={matchn} sp={w} sp2 = {i} ref={boxid} changematches={this.changematches.bind(this)}/>
             )
+            w++;
           }
           else
           {
@@ -87,6 +90,11 @@ export default class SingleDisplay extends Component {
             )
           }
       }
+        if (spacing == 0)
+        {
+          var sty = {top: i*50*Math.pow(2,matchn-1)+spacing + "px", position: "absolute", height: "95px", width: "30px"};
+          boxes.push(<div style={sty} />);
+        }
         spacing = spacing+Math.pow(2, matchn-1)*25;
         matchn++;
         roundparticipants = roundparticipants/2;
@@ -101,7 +109,6 @@ export default class SingleDisplay extends Component {
     this.refs[id2].closs();
     this.refs[id3].ctrue();
     this.refs[id3].cval(val);
-    this.refs[this.refs[id3].gopponent()].ctrue();
   }
 
   render() {
