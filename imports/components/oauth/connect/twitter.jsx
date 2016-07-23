cba67b88c1db304790d3d1644c6b825f103bb4c2
Ayato French <ayato@brachyon.com>
2016-07-22 20:07:48 -0700

    Added Oauth for Twitch, Facebook and Twitter. Google and Discord will be added in a feature version. God Speed...

---
+++ imports/components/oauth/connect/twitter.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import FontAwesome from "react-fontawesome";
+
+export default class TwitterConnect extends Component {
+
+  onClick(e) {
+    e.preventDefault();
+    Meteor.linkWithTwitter({
+    }, function(err){
+      if(err){
+        console.log(err);
+        toastr.error(err.reason, "Error!");
+      }
+      else {
+        toastr.success("Integrated with Twitter!", "Success!");
+      }
+    })
+  }
+
+  render() {
+    if(Meteor.user().services.twitter == null) {
+      return (
+        <button onClick={this.onClick.bind(this)}>
+          <FontAwesome style={{marginRight: 10}} name="twitter" />
+          Connect to Twitter
+        </button>
+      );
+    }
+    else {
+      return (
+        <div>Already connected to Twitter!</div>
+      )
+    }
+  }
+}
