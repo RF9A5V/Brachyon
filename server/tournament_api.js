b5f543201c9ab2c82bb5e281bba206baecc811f4
axman36 <alex@renefe.com>
2016-08-08 16:30:16 -0700

    Losers bracket is now on the backend

--- server/tournament_api.js
+++ server/tournament_api.js
@@ -82,13 +82,15 @@ var OrganizeSuite = {
       }
       rounds.push(temp);
     }
-    return rounds;
+    var frounds = [];
+    frounds.push(rounds);
+    return frounds;
   },
 
 
   doubleElim: function(participants) {
     frounds = Array(2);
-    frounds[0] = OrganizeSuite.singleElim(participants);
+    sinround = OrganizeSuite.singleElim(participants)[0];
     var roundCount = Math.ceil(Math.log(participants.length) / Math.log(2));
     var HalfPoint = (Math.pow(2, roundCount-1) + Math.pow(2, roundCount-2));
     var ovhalf = participants.length > HalfPoint ? true:false;
@@ -159,12 +161,15 @@ var OrganizeSuite = {
           }
           else if (i == 1)
           {
+            console.log(frounds[0][0]);
             while (!(frounds[0][0][q].playerOne)) //Find a valid bye in the winners bracket as this set of byes goes orderly down the list.
             {
               q++;
+              console.log(q);
               if (q >= frounds[0][0].length) //For every valid bye, there is a valid winners bracket bye.
                 throw error;
             }
+            console.log("heh");
             frounds[0][1][frounds[0][1].length-Math.floor(q/2)-1].losr = i;
             frounds[0][1][frounds[0][1].length-Math.floor(q/2)-1].losm = j;
             if (frounds[0][0][q].losm == null) //These sets are left empty in the scenario where the first set of byes use them.
@@ -230,7 +235,8 @@ var OrganizeSuite = {
       }
     }
 
-    return frounds[0];
+    console.log(frounds[1]);
+    return frounds;
   }
 }
 
