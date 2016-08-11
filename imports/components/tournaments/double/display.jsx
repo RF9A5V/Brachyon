0de0a72d88d16273bca372bc70e26689717cf2d9
axman36 <alex@renefe.com>
2016-08-10 17:09:38 -0700

    Double Elim Finals implemented

--- imports/components/tournaments/double/display.jsx
+++ imports/components/tournaments/double/display.jsx
@@ -8,16 +8,28 @@ export default class DoubleDisplay extends Component {
         <div className="col">
           <div className="row">
             { //Winner's bracket block
-              Array(this.props.rounds[0].length).fill("").map((_, i) => {
-                return (
-                  <div style={{width: 150, textAlign: "center"}}>
-                    Round { i + 1 }
-                  </div>
-                )
+              Array(this.props.rounds[0].length+2).fill("").map((_, i) => {
+                if (i < this.props.rounds[0].length)
+                {
+                  return (
+                    <div style={{width: 150, textAlign: "center"}}>
+                      Round { i + 1 }
+                    </div>
+                  )
+                }
+                else
+                {
+                  return (
+                    <div style={{width: 150, textAlign: "center"}}>
+                      Final Round {i + 1 - this.props.rounds[0].length}
+                    </div>
+                  )
+                }
               })
             }
           </div>
           <div className="row">
+            <div className="row">
             {
               this.props.rounds[0].map((round, i) => {
                 return (
@@ -47,6 +59,38 @@ export default class DoubleDisplay extends Component {
                 );
               })
             }
+            </div>
+            <div className="row">
+            {
+              this.props.rounds[2].map((round, i) => {
+                return (
+                  <div className="col" style={{justifyContent: "space-around"}}>
+                    {
+                      round.map((match, j) => {
+                        var isFutureLoser = false;
+                        if(i < this.props.rounds[2].length - 1){
+                          var nextMatch = this.props.rounds[2][i + 1][Math.floor(j / 2)];
+                          var rNum = i + 1;
+                          var mNum = Math.floor(j / 2);
+                          while(++rNum < this.props.rounds[2].length && nextMatch.winner != null) {
+                            if(nextMatch.winner != match.playerOne && nextMatch.winner != match.playerTwo) {
+                              isFutureLoser = true;
+                              break;
+                            }
+                            mNum = Math.floor(mNum / 2);
+                            nextMatch = this.props.rounds[2][rNum][mNum];
+                          }
+                        }
+                        return (
+                          <MatchBlock match={match} bracket={2} roundNumber={i} matchNumber={j} roundSize={this.props.rounds[2].length} id={this.props.id} isFutureLoser={isFutureLoser} />
+                        );
+                      })
+                    }
+                  </div>
+                );
+              })
+            }
+            </div>
           </div>
         </div>
 
