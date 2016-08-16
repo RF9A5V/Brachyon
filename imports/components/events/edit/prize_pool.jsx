04e3be62a6bd37ac690d1a2af97817ae562dcec3
Steven Chen <steven@thecrudeintellectual.com>
2016-08-16 15:31:27 -0700

    Prize pool sliders work with labels

--- imports/components/events/edit/prize_pool.jsx
+++ imports/components/events/edit/prize_pool.jsx
@@ -6,7 +6,42 @@ export default class PrizePoolBreakdown extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      labels: [
+        {
+          name: "First Place",
+          start: 0
+        }
+      ]
+    }
+  }
 
+  onLabelCreate(e){
+    e.preventDefault();
+
+    if(this.refs.field.value == ""){
+      return toastr.error("Label needs name.", "Error!");
+    }
+    if(this.refs.start.value == ""){
+      return toastr.error("Label needs starting position.", "Error!");
+    }
+    var start = parseInt(this.refs.start.value);
+    var end = this.refs.end.value;
+    if(end == "") {
+      end = undefined;
+    }
+    else {
+      end = parseInt(end);
+    }
+    this.state.labels.push({
+      name: this.refs.field.value,
+      start,
+      end,
+      percentage: 0,
+      position: 0
+    });
+    toastr.success("Added label to prize pools.", "Success!");
+    this.forceUpdate();
   }
 
   render() {
@@ -24,10 +59,20 @@ export default class PrizePoolBreakdown extends Component {
           event.organize.map((bracket, i) => {
             return (
               <div className="col" key={i}>
-                <SliderBars />
-                <div>
-                  <button>Add a Label</button>
+                <div className="col">
+                  <label>Need vocab</label>
+                  <input type="text" ref="field" style={{margin: 0}} />
+                  <label>Placement</label>
+                  <div className="row">
+                    <input style={{margin: 0, marginRight: 10}} ref="start" />
+                    <input ref="end" style={{margin: 0}} />
+                  </div>
+                  <div>
+                    <button onClick={this.onLabelCreate.bind(this)}>Add a Label</button>
+                  </div>
                 </div>
+                <SliderBars labels={this.state.labels} />
+
               </div>
             )
           })
