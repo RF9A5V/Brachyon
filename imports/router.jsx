4a59693b1d383eab8bec3c80a2606fd891c04f2f
Steven Chen <steven@thecrudeintellectual.com>
2016-08-21 13:57:14 -0700

    Added a quick mockup of maybe what our compendium looks like.

--- imports/router.jsx
+++ imports/router.jsx
@@ -22,6 +22,7 @@ import UserOptionsScreen from "../imports/components/users/options.jsx";
 import CurrencyPurchaseScreen from "../imports/components/public/currency_purchase.jsx";
 import ShowEventScreen from "../imports/components/events/show.jsx";
 import BracketShowScreen from "../imports/components/events/brackets/show.jsx";
+import BookScreen from "../imports/components/test/book.jsx";
 
 function isLoggedIn(nextState, replace){
   if(Meteor.userId()){
@@ -58,6 +59,7 @@ export const renderRoutes = () => (
       <Route path="tournaments/double" component={TournamentDoubleScreen} />
       <Route path="buy_currency" component={CurrencyPurchaseScreen} />
       <Route path="events/:eventId/brackets/:bracketIndex" component={BracketShowScreen} />
+      <Route path="test" component={BookScreen} />
     </Route>
   </Router>
 )
