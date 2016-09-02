175163fc3ca37fb36b14754f3071b4caa4dd8f32
Steven Chen <steven@thecrudeintellectual.com>
2016-09-01 19:00:29 -0700

    Date range works for event searches

--- imports/components/events/discover/search/date_filter.jsx
+++ imports/components/events/discover/search/date_filter.jsx
@@ -8,16 +8,46 @@ export default class DateFilter extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      useTime: false
+      useTime: false,
+      useRange: false,
+      startDate: null,
+      startTime: null,
+      endDate: null,
+      endTime: null
     };
   }
 
-  onDateChange() {
-    var date = this.refs.date.value();
-    console.log(date);
+  parentChangeWrapper(){
     this.props.onChange({
-      date
+      startDate: {
+        date: this.state.startDate,
+        time: this.state.startTime
+      },
+      endDate: {
+        date: this.state.endDate,
+        time: this.state.endTime
+      }
     });
+  }
+
+  onStartDateChange() {
+    this.state.startDate = this.refs.date.value();
+    this.parentChangeWrapper();
+  }
+
+  onStartTimeChange() {
+    this.state.startTime = this.refs.time.value();
+    this.parentChangeWrapper();
+  }
+
+  onEndDateChange() {
+    this.state.endDate = this.refs.endDate.value();
+    this.parentChangeWrapper();
+  }
+
+  onEndTimeChange() {
+    this.state.endTime = this.refs.endTime.value();
+    this.parentChangeWrapper();
   }
 
   render() {
@@ -26,8 +56,8 @@ export default class DateFilter extends Component {
         <div className="col" style={{marginRight: 10}}>
           <label>Date</label>
           <div className="row">
-            <div style={{marginRight: 10}}>
-              <DateInput ref="date" onChange={this.onDateChange.bind(this)} />
+            <div>
+              <DateInput ref="date" onChange={this.onStartDateChange.bind(this)} />
             </div>
           </div>
         </div>
@@ -36,7 +66,27 @@ export default class DateFilter extends Component {
             <div className="col" style={{marginRight: 10}}>
               <label>Time</label>
               <div>
-                <TimeInput ref="time" />
+                <TimeInput ref="time" onChange={this.onStartTimeChange.bind(this)} />
+              </div>
+            </div>
+          ) : (
+            ""
+          )
+        }
+        {
+          this.state.useRange ? (
+            <div className="row">
+              <div className="col" style={{marginRight: 10}}>
+                <label>End Date</label>
+                <div>
+                  <DateInput ref="endDate" onChange={this.onEndDateChange.bind(this)} />
+                </div>
+              </div>
+              <div className="col" style={{marginRight: 10}}>
+                <label>End Time</label>
+                <div>
+                  <TimeInput ref="endTime" onChange={this.onEndTimeChange.bind(this)} />
+                </div>
               </div>
             </div>
           ) : (
@@ -49,6 +99,22 @@ export default class DateFilter extends Component {
               "Remove Time"
             ) : (
               "Use Time"
+            )
+          }
+        </button>
+        <button onClick={() => {
+          this.state.useRange = !this.state.useRange;
+          if(!this.state.useRange) {
+            this.state.endDate = null;
+            this.state.endTime = null;
+          }
+          this.forceUpdate();
+        }}  style={{marginBottom: 2.5, marginLeft: 10}}>
+          {
+            this.state.useRange ? (
+              "Remove Date Range"
+            ) : (
+              "Set Date Range"
             )
           }
         </button>
