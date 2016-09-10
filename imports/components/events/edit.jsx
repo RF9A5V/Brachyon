dffe86e96d85fcfb9a53e12311f4eec4becd6db6
Steven Chen <steven@thecrudeintellectual.com>
2016-09-10 16:46:16 -0700

    Integrated new side tabs to edit event process

--- imports/components/events/edit.jsx
+++ imports/components/events/edit.jsx
@@ -1,16 +1,32 @@
 import React from "react";
 import TrackerReact from "meteor/ultimatejs:tracker-react";
 
-import EditMenu from "./edit/edit_menu.jsx";
-
 import LoadingScreen from "../public/loading.jsx";
+
+import TabController from "/imports/components/public/side_tabs/tab_controller.jsx";
+
+import Main from "./modules/main.jsx";
+
+import StaffPage from "./modules/organize/staff.jsx";
+
+import BracketMain from "./modules/bracket/edit.jsx";
+import AddBracket from "./modules/bracket/add.jsx";
+import EditBracket from "./modules/bracket/edit.jsx";
+
+import DescriptionPage from "./modules/details/description.jsx";
+import LocationPage from "./modules/details/location.jsx";
+import DatetimePage from "./modules/details/datetime.jsx";
+import ImagePage from "./modules/details/image.jsx";
+
+import RevenueDetailsPage from "./modules/revenue/details.jsx";
+import TicketsPage from "./modules/revenue/tickets.jsx";
+import TierPage from "./modules/revenue/tiers.jsx";
 
 export default class EditEventScreen extends TrackerReact(React.Component){
 
   constructor(props) {
     super(props);
     this.state = {
-      detailsSuite: {},
       event: Meteor.subscribe("event", this.props.params.eventId)
     }
   }
@@ -19,8 +35,108 @@ export default class EditEventScreen extends TrackerReact(React.Component){
     this.state.event.stop();
   }
 
-  event() {
-    return Events.find().fetch()[0];
+  items() {
+    var event = Events.findOne();
+    return [
+      {
+        text: "Organize",
+        icon: "bullhorn",
+        subitems: [
+          {
+            component: Main,
+            args: {
+              name: "Organize"
+            }
+          },
+          {
+            text: "Staff",
+            component: StaffPage
+          }
+        ]
+      },
+      {
+        text: "Brackets",
+        icon: "gamepad",
+        subitems: [
+          {
+            component: Main,
+            args: {
+              name: "Brackets"
+            }
+          }
+        ].concat((event.brackets ? (
+          event.brackets.map((bracket, index) => {
+            return {
+              component: EditBracket,
+              text: bracket.name,
+              args: {
+                index,
+                bracket
+              }
+            }
+          })
+        ) : (
+          []
+        ))).concat([
+          {
+            component: AddBracket,
+            text: "Add Bracket"
+          }
+        ])
+      },
+      {
+        text: "Details",
+        icon: "file-text",
+        subitems: [
+          {
+            component: Main,
+            args: {
+              name: "Details"
+            }
+          },
+          {
+            component: DescriptionPage,
+            text: "Description"
+          },
+          {
+            component: LocationPage,
+            text: "Location"
+          },
+          {
+            component: DatetimePage,
+            text: "Date and Time"
+          },
+          {
+            component: ImagePage,
+            text: "Banner"
+          }
+        ]
+      },
+      {
+        text: "Revenue",
+        icon: "money",
+        subitems: [
+          {
+            component: Main,
+            args: {
+              name: "Revenue"
+            }
+          },
+          {
+            component: RevenueDetailsPage,
+            text: "Details"
+          },
+          {
+            component: TicketsPage,
+            text: "Tickets"
+          },
+          {
+            component: TierPage,
+            text: "Tiers"
+          }
+        ]
+      }
+    ]
   }
 
   render() {
@@ -31,9 +147,7 @@ export default class EditEventScreen extends TrackerReact(React.Component){
     }
     else {
       return (
-        <div className="col">
-          <EditMenu event={this.event()} />
-        </div>
+        <TabController items={this.items()} />
       )
     }
   }
