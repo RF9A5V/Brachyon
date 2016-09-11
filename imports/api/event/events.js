0e01b59bf63d336aa938156915e2440327d90ec8
Steven Chen <steven@thecrudeintellectual.com>
2016-09-10 18:01:31 -0700

    Fixed references to bracket module as organize module

--- imports/api/event/events.js
+++ imports/api/event/events.js
@@ -5,7 +5,10 @@ export default Events = new Mongo.Collection('events', {
   transform: (doc) => {
     var games = [];
     if(doc.details.banner){
-      doc.bannerUrl = Images.findOne(doc.details.banner).url();
+      var img = Images.findOne(doc.details.banner);
+      if(img != null) {
+        doc.bannerUrl = img.url();
+      }
     }
     doc.games = Games.find({_id: {$in: games}});
     return doc;
