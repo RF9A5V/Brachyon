e3d0ec724ed5e16b51fe3ab5b1b36521693db168
Steven Chen <steven@thecrudeintellectual.com>
2016-09-19 20:37:33 -0700

    Profile images verified working with new system

--- server/main.js
+++ server/main.js
@@ -1,9 +1,9 @@
 import { Meteor } from 'meteor/meteor';
 import Events from '/imports/api/event/events.js';
-import Images from '/imports/api/event/images.js';
+import { ProfileImages } from "/imports/api/users/profile_images.js";
+import { Images } from "/imports/api/event/images.js";
 import Games from '/imports/api/games/games.js';
 import Icons from '/imports/api/sponsorship/icon.js';
-import ProfileImages from '/imports/api/users/profile_images.js';
 import ProfileBanners from "/imports/api/users/profile_banners.js";
 
 Events._ensureIndex({
@@ -79,37 +79,7 @@ Meteor.startup(() => {
 
   SyncedCron.start();
 
-  Images.allow({
-    insert: function() {
-      return true;
-    },
-    update:function(userId,project,fields,modifier){
-     return true;
-    },
-    remove:function(userId,project){
-      return true;
-    },
-    download:function(){
-      return true;
-    }
-  })
-
   Icons.allow({
-    insert: function() {
-      return true;
-    },
-    update:function(userId,project,fields,modifier){
-     return true;
-    },
-    remove:function(userId,project){
-      return true;
-    },
-    download:function(){
-      return true;
-    }
-  })
-
-  ProfileImages.allow({
     insert: function() {
       return true;
     },
