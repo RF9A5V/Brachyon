dadd02da00ef284518303727c90f653b3ac870e8
Steven Chen <steven@thecrudeintellectual.com>
2016-10-14 10:32:19 -0700

    Added validation checks to event start date and crowdfunding due date

--- server/events/modules/cf/details.js
+++ server/events/modules/cf/details.js
@@ -1,9 +1,23 @@
+import moment from "moment";
+
 Meteor.methods({
   "events.crowdfunding.saveDetails"(id, amount, dueDate){
     var event = Events.findOne(id);
     if(!event) {
       throw new Meteor.Error(404, "Event not found.");
     }
+    if(amount < 0) {
+      throw new Meteor.Error(403, "Can't use a negative number for crowdfunding!");
+    }
+    var eventStartDate = moment(event.details.datetime).hour(0).minute(0).second(0).millisecond(0);
+    var date = moment(dueDate).hour(0).minute(0).second(0).millisecond(0);
+
+    date.dayOfYear(date.dayOfYear() + 5);
+
+    if(date.isAfter(eventStartDate)) {
+      throw new Meteor.Error(403, "Cannot crowdfund event unless due date is at least five days before event start!");
+    }
+
     Events.update(id, {
       $set: {
         "crowdfunding.details.amount": amount,
