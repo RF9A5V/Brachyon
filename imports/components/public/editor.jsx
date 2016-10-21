39dd075e99198efb655baf3c26d48a7fc3edf3d0
Steven Chen <steven@thecrudeintellectual.com>
2016-10-21 15:13:10 -0700

    Adding media for description works

--- imports/components/public/editor.jsx
+++ imports/components/public/editor.jsx
@@ -1,5 +1,15 @@
 import React, { Component } from "react";
-import { Editor, EditorState, RichUtils, ContentState, convertToRaw, convertFromRaw } from 'draft-js';
+import { Editor, EditorState, RichUtils, ContentState, convertToRaw, convertFromRaw, Modifier, Entity } from 'draft-js';
+import Immutable from "immutable";
+
+import LinkEntity from "./editor/link.jsx";
+import LinkBlock from "./editor/link_block.jsx";
+
+import ImageEntity from "./editor/img.jsx";
+import ImageBlock from "./editor/img_block.jsx";
+
+import VideoEntity from "./editor/video.jsx";
+import VideoBlock from "./editor/video_block.jsx";
 
 export default class BrachyonEditor extends Component {
   constructor(props) {
@@ -13,7 +23,12 @@ export default class BrachyonEditor extends Component {
     }
     this.state = {
       editorState: state,
-      isBold: false
+      isBold: false,
+      isItalic: false,
+      isStrike: false,
+      isLinkOpen: false,
+      isImgOpen: false,
+      isVidOpen: false
     };
   }
 
@@ -29,26 +44,133 @@ export default class BrachyonEditor extends Component {
   }
 
   toggleBold() {
-    this.state.isBold = !this.state.isBold;
     this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, "BOLD"));
+    this.setState({
+      isBold: !this.state.isBold
+    })
   }
 
-  setBlockClasses(content) {
-    const type = content.getType();
-    if(type == "div") {
-      return "edit-line"
+  toggleItalic() {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, "ITALIC"));
+    this.setState({
+      isItalic: !this.state.isItalic
+    })
+  }
+
+  toggleStrike() {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, "STRIKETHROUGH"));
+    this.setState({
+      isStrike: !this.state.isStrike
+    })
+  }
+
+  editorButtons() {
+    return (
+      <div className="row x-center">
+        <div className={`editor-button ${this.state.isBold ? "active" : ""}`} style={{fontWeight: "bold"}} onClick={this.toggleBold.bind(this)}>
+          Bold
+        </div>
+        <div className={`editor-button ${this.state.isItalic ? "active" : ""}`} style={{fontStyle: "italic"}} onClick={this.toggleItalic.bind(this)}>
+          Italic
+        </div>
+        <div className={`editor-button ${this.state.isStrike ? "active" : ""}`} style={{textDecoration: "line-through"}} onClick={this.toggleStrike.bind(this)}>
+          Strike
+        </div>
+        <div style={{width: 2.5, height: 45, backgroundColor: "white", marginRight: 10}}>
+        </div>
+        <div className="editor-button" onClick={() => { this.setState({isLinkOpen: true}) }}>
+          Link
+        </div>
+        <div className="editor-button" onClick={() => { this.setState({ isImgOpen: true }) }}>
+          Image
+        </div>
+        <div className="editor-button" onClick={() => { this.setState({ isVidOpen: true }) }}>
+          Video
+        </div>
+        <div style={{width: 2.5, height: 45, backgroundColor: "white", marginRight: 10}}>
+        </div>
+        <div className="editor-button" onClick={ () => {
+          var blocks = convertToRaw(this.state.editorState.getCurrentContent());
+          blocks.blocks.forEach((bloc) => {
+            console.log(bloc);
+          })
+        } }>
+          Log State
+        </div>
+      </div>
+    )
+  }
+
+  blockRenderer(contentBlock) {
+    const type = contentBlock.getType();
+    if(type == "atomic") {
+      console.log(contentBlock.getEntityAt(0));
+      var entityKey = contentBlock.getEntityAt(0);
+      if(entityKey == null) {
+        return null;
+      }
+      var entity = Entity.get(contentBlock.getEntityAt(0)).getData();
+      if(entity.type == "LINK") {
+        return {
+          component: LinkBlock,
+          editable: true,
+          props: {
+            text: contentBlock.getText(),
+            href: entity.href
+          }
+        }
+      }
+      if(entity.type == "IMAGE") {
+        return {
+          component: ImageBlock,
+          editable: true,
+          props: {
+            src: entity.src
+          }
+        }
+      }
+      if(entity.type == "VIDEO") {
+        return {
+          component: VideoBlock,
+          editable: true,
+          props: {
+            iframe: entity.iframe
+          }
+        }
+      }
+
     }
+    return null;
   }
 
   render() {
     return (
       <div>
-        <div className="row x-center">
-          <div className={`editor-button ${this.state.isBold ? "active" : ""}`} onClick={this.toggleBold.bind(this)}>
-            B
-          </div>
-        </div>
-        <Editor editorState={this.state.editorState} onChange={this.onChange.bind(this)} blockStyleFn={this.setBlockClasses.bind(this)}/>
+        {
+          this.editorButtons()
+        }
+        <Editor editorState={this.state.editorState} onChange={this.onChange.bind(this)} blockRendererFn={this.blockRenderer} />
+        {
+          this.state.isLinkOpen ? (
+            <LinkEntity editorRef={this} />
+          ) : (
+            ""
+          )
+        }
+        {
+          this.state.isImgOpen ? (
+            <ImageEntity editorRef={this} />
+          ) : (
+            ""
+          )
+        }
+        {
+          this.state.isVidOpen ? (
+            <VideoEntity editorRef={this} />
+          ) : (
+            ""
+          )
+        }
       </div>
     );
   }
