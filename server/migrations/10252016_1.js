c491574984661a5d1f6b846f6781ca30922ddbbe
Steven Chen <steven@thecrudeintellectual.com>
2016-10-25 13:16:08 -0700

    Added migration tool for DB

---
+++ server/migrations/10252016_1.js
@@ -0,0 +1,26 @@
+import { ProfileImages } from "/imports/api/users/profile_images.js";
+
+Migrations.add({
+  version: 1,
+  up: function() {
+    var usersWithImage = Meteor.users.find({"profile.image": { $ne: null }}, { _id: 1, "profile.image": 1 });
+    usersWithImage.forEach((usr) => {
+      var img = ProfileImages.findOne(usr.profile.image);
+      Meteor.users.update(usr._id, {
+        $set: {
+          "profile.imageUrl": img.link()
+        }
+      });
+    });
+  },
+  down: function() {
+    var usersWithImage = Meteor.users.find({"profile.imageUrl": { $ne: null }}, { _id: 1, "profile.image": 1 });
+    usersWithImage.forEach(usr => {
+      Meteor.users.update(usr._id, {
+        $set: {
+          "profile.imageUrl": null
+        }
+      })
+    })
+  }
+})
