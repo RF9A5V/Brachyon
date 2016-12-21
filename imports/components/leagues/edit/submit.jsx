16bb891367de502804f006e1f2fea7fb808e95df
Steven Chen <steven@brachyon.com>
2016-12-21 13:25:07 -0800

    Edit backend should also be done

--- imports/components/leagues/edit/submit.jsx
+++ imports/components/leagues/edit/submit.jsx
@@ -1,16 +1,40 @@
 import React, { Component } from "react";
+import { browserHistory } from "react-router";
+
+import Leagues from "/imports/api/leagues/league.js";
 
 export default class SubmitPanel extends Component {
 
   constructor(props) {
     super(props);
-    console.log(props.changelog);
+    var league = Leagues.findOne();
+    Meteor.call("leagues.edit", league.slug, props.changelog, (err) => {
+      if(err) {
+        toastr.error(err.reason, "Error!");
+      }
+      else {
+        toastr.success("Successfully edited league!", "Success!");
+
+        this.setState({
+          success: true
+        })
+      }
+    });
+    this.state = {
+      success: false
+    }
   }
 
   render() {
     return (
       <div>
-        Submit this shiz
+        {
+          this.state.success ? (
+            "Updated League"
+          ) : (
+            "Loading..."
+          )
+        }
       </div>
     )
   }
