9961b42ce0078413c0e2a402602e8001c3f8d08f
jorgekina <achira92@gmail.com>
2017-01-03 14:39:36 -0800

    redesign of swiss/round robbin. and user options

--- imports/components/tournaments/swiss/display.jsx
+++ imports/components/tournaments/swiss/display.jsx
@@ -126,7 +126,7 @@ export default class SwissDisplay extends TrackerReact(Component) {
     return (
       <div className="col">
         <div className="row center x-center">
-          <h2>{this.state.page == 0 ? "Leaderboard" : "Round " + (this.state.page)}</h2>
+          <h3>{this.state.page == 0 ? "Leaderboard" : "Round " + (this.state.page)}</h3>
         </div>
         <div className="row swiss-tabs">
           <div className={`swiss-tab-header ${this.state.page == 0 ? "active" : ""}`} onClick={() => { this.setState({ page: 0 }) }}>
@@ -162,8 +162,8 @@ export default class SwissDisplay extends TrackerReact(Component) {
                   </div>
                   {
                     this.props.rounds[this.props.rounds.length-1].players.sort((a, b) => {
-                    return b.score - a.score;
-                    }).map((playerObj, i) => {
+                      return b.score - a.score;
+                      }).map((playerObj, i) => {
                       return (
                         <div className="row swiss-row">
                           <div className="swiss-entry">
