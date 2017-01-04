979e120eb0b604352b63ca1e1e1506eff8527321
Steven Chen <steven@brachyon.com>
2017-01-04 12:25:32 -0800

    Initial push for game hubs

---
+++ imports/components/sandbox/overview.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class OverviewPanel extends Component {
+  render() {
+    return (
+      <div>
+        Hello!
+      </div>
+    )
+  }
+}
