c887570c3e08e015a4b2d3ba09173c9be26f2e99
Steven Chen <steven@brachyon.com>
2017-01-06 15:43:26 -0800

    Updated styles for org create

--- imports/components/organizations/create.jsx
+++ imports/components/organizations/create.jsx
@@ -89,19 +89,25 @@ export default class OrganizationCreateScreen extends Component {
   render() {
     return(
       <div>
-        <h3>Create Organization</h3>
-        <div className="col" style={{padding: 20}}>
-          <div className="row x-center">
-            <h5>Name</h5>
-            <input ref="organizationName" type="text" placeholder="Organization Name"/>
+        <div className="col" style={{padding: 20, width: "75%", margin: "0 auto"}}>
+          <h3 style={{marginBottom: 20, textAlign: "center"}}>Create Organization</h3>
+          <h5>Name</h5>
+          <input ref="organizationName" type="text" placeholder="Organization Name"/>
+          <div className="row">
+            <div className="col col-1 x-center">
+              <h5>Profile</h5>
+              <ImageForm ref="profile" collection={OrgImages} onImgSelected={this.setImage.bind(this)} defaultImage={this.state.image} />
+            </div>
+            <div className="col col-1 x-center">
+              <h5>Banner</h5>
+              <ImageForm ref="banner" collection={OrgBanners} aspectRatio={16/9} onImgSelected={this.setBanner.bind(this)} defaultImage={this.state.banner} />
+            </div>
           </div>
-          <h5>Profile</h5>
-          <ImageForm ref="profile" collection={OrgImages} onImgSelected={this.setImage.bind(this)} defaultImage={this.state.image} />
-          <h5>Banner</h5>
-          <ImageForm ref="banner" collection={OrgBanners} aspectRatio={16/9} onImgSelected={this.setBanner.bind(this)} defaultImage={this.state.banner} />
           <h5>Description</h5>
           <Editor onChange={this.setDescriptionState.bind(this)} useInsert={true} usePara={true} useTable={true} />
-          <button onClick={this.submit.bind(this)}>Create</button>
+          <div className="row center" style={{marginTop: 20}}>
+            <button onClick={this.submit.bind(this)}>Create</button>
+          </div>
         </div>
       </div>
     )
