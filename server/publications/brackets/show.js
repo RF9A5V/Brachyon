02b29ed8bf22e09f533cfa1ae143fdc8f1427457
Steven Chen <steven@brachyon.com>
2017-01-14 13:04:17 -0800

    HOTFIX: Add participant subscription to bracketContainer

--- server/publications/brackets/show.js
+++ server/publications/brackets/show.js
@@ -12,10 +12,13 @@ Meteor.publish("bracketContainer", (_id) => {
   if(!instance) {
     return [];
   }
+  var partIds = [];
+  instance.brackets.forEach(b => { partIds = partIds.concat(b.participants.map(p => { return p.id })) });
   return [
     Instances.find({_id}),
     Brackets.find({ _id: {
       $in: instance.brackets.map(obj => { obj.id })
-    }})
+    }}),
+    Meteor.users.find({ _id: { $in: partIds } })
   ]
 })
