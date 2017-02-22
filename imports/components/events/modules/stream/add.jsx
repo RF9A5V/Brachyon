bc3cf2a98e52254cdfc35ea6ccdae0f57300711d
Steven Chen <steven@brachyon.com>
2017-02-21 16:16:42 -0800

    Edit process first runthrought done

--- imports/components/events/modules/stream/add.jsx
+++ imports/components/events/modules/stream/add.jsx
@@ -9,29 +9,18 @@ export default class AddStream extends Component {
     }
   }
 
-  onStreamNameSave(){
-    Meteor.call("events.updateTwitchStream", this.state.id, this.refs.twitchStreamName.value, (err)=> {
-      if(err){
-        toastr.error(err.reason, "Error!");
-      }
-      else {
-        toastr.success("Successfully updated Twitch iFrame!", "Success!");
-      }
-    })
+  value() {
+    return this.refs.twitchStreamName.value
   }
-
 
   render(){
     return(
-      <div className="col" style={{width: "50%", minWidth: 300, margin: "0 auto"}}>
+      <div className="col">
         <h4>Stream</h4>
         <div className="submodule-bg">
           <div className="row center x-center">
             <span>https://twitch.tv/</span>
-            <input ref="twitchStreamName" placeholder="Twitch Username" defaultValue={(Events.findOne().twitchStream || {}).name} />
-          </div>
-          <div className="row center x-center">
-            <button onClick={this.onStreamNameSave.bind(this)}>Save</button>
+            <input ref="twitchStreamName" placeholder="Twitch Username" defaultValue={(Events.findOne().stream || {}).twitchStream} />
           </div>
         </div>
       </div>
