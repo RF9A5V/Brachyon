4eedc07c26f31e2052eef33259ca6ee04f335abb
Steven Chen <steven@brachyon.com>
2017-02-23 17:20:07 -0800

    Copy and pasting the description removes the default styling

--- imports/components/events/preview/slides/description.jsx
+++ imports/components/events/preview/slides/description.jsx
@@ -66,7 +66,10 @@ export default class Description extends Component {
                 mq.matches ? (
                   ""
                 ) : (
-                  <div className="slide-description col-3" style={{overflowY: "auto", textAlign: "left"}}>
+                  <div className="slide-description col-3" style={{overflowY: "auto", textAlign: "left"}} onWheel={(e) => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                  }}>
                     <div dangerouslySetInnerHTML={{__html: event.details.description}}>
                     </div>
                   </div>
