3fa7d90d228be09436f9113f23f49ef197a85354
Steven Chen <steven@brachyon.com>
2017-02-27 21:16:56 -0800

    Removed twitter file

--- server/api/twitter.js
+++
@@ -1,8 +0,0 @@
-import twitterAPI from "node-twitter-api";
-
-const tweetHandler = new twitterAPI({
-  consumerKey: Meteor.settings.private.consumerKey,
-  consumerSecret: Meteor.settings.private.consumerSecret
-});
-
-export default tweetHandler;
