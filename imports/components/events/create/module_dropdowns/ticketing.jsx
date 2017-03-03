8346986b480c0db06e58853f044f1d45b0c2301a
Steven Chen <steven@brachyon.com>
2017-03-03 12:05:46 -0800

    Added discounts to ticketing process on create

--- imports/components/events/create/module_dropdowns/ticketing.jsx
+++ imports/components/events/create/module_dropdowns/ticketing.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
+import FontAwesome from "react-fontawesome";
 
 export default class TicketingPanel extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      paymentType: "onsite"
+      paymentType: "onsite",
+      discounts: {},
+      free: {}
     }
   }
 
@@ -13,14 +16,28 @@ export default class TicketingPanel extends Component {
     var obj = {};
     obj["venue"] = parseInt(parseFloat(this.refs.venue.value) * 100);
     this.props.brackets.forEach((b, i) => {
-      obj[i] = parseInt(parseFloat(this.refs[b].value) * 100);
+      if(this.state.free[b]) {
+        obj[i] = {
+          price: 0
+        }
+      }
+      else {
+        obj[i] = {
+          price: parseInt(parseFloat(this.refs[b].value) * 100),
+          discounts: (this.state.discounts[b] || []).map(j => {
+            return {
+              name: this.refs[b + "_discountName_" + j].value,
+              price: parseInt(parseFloat(this.refs[b + "_discountPrice_" + j].value) * 100)
+            }
+          })
+        }
+      }
     });
     obj.paymentType = this.state.paymentType;
     return obj;
   }
 
   render() {
-    console.log(this.props);
     if(this.props.status) {
       return (
         <div className="col">
@@ -35,13 +52,84 @@ export default class TicketingPanel extends Component {
           {
             this.props.brackets.map((id, i) => {
               return (
-                <div className="row x-center" style={{marginBottom: 10}} key={id}>
-                  <div className="col-1">
-                    Bracket {i + 1}
+                <div className="col" style={{marginBottom: 10, padding: 10, border: "solid 2px #666"}} key={id}>
+                  <div className="row x-center" style={{marginBottom: 10}}>
+                    <div className="col-1">
+                      Bracket {i + 1}
+                    </div>
+                    <div className="row col-1 x-center">
+                      {
+                        this.state.free[id] ? (
+                          <div style={{margin: "0 10px"}}>Free</div>
+                        ) : (
+                          <div>
+                            $<input ref={id} type="number" style={{margin: "0 10px"}} defaultValue={0} />
+                          </div>
+                        )
+                      }
+                      <div className="row x-center">
+                        <input type="checkbox" style={{margin: 0, marginRight: 10}} checked={this.state.free[id] == true} onClick={() => {
+                          if(this.state.free[id]) {
+                            delete this.state.free[id];
+                          }
+                          else {
+                            this.state.free[id] = true;
+                            delete this.state.discounts[id];
+                          }
+                          this.forceUpdate();
+                        }} />
+                        <span>
+                          Is this bracket free?
+                        </span>
+                      </div>
+                    </div>
                   </div>
-                  <div className="col-1">
-                    $<input ref={id} type="number" style={{margin: 0, marginLeft: 10}} defaultValue={0} />
-                  </div>
+                  {
+                    this.state.free[id] ? (
+                      null
+                    ) : (
+                      <hr className="user-divider" />
+                    )
+                  }
+                  {
+                    this.state.discounts[id] ? (
+                      <div className="row center">
+                        <label>Discounts</label>
+                      </div>
+                    ) : (
+                      null
+                    )
+                  }
+                  {
+                    (this.state.discounts[id] || []).map((val, i) => {
+                      return (
+                        <div className="row x-center" style={{marginBottom: 10}} key={id + "_" + val}>
+                          <div className="col-1">
+                            <input type="text" ref={id + "_discountName_" + i} style={{margin: 0, marginRight: 10}} defaultValue={""} placeholder="Discount Name" />
+                          </div>
+                          <div className="row x-center col-1">
+                            $<input type="number" ref={id + "_discountPrice_" + i} style={{margin: "0 10px"}} defaultValue={0} />
+                            <FontAwesome size="2x" name="times" onClick={() => {
+                              this.state.discounts[id].splice(i, 1);
+                              this.forceUpdate();
+                            }} />
+                          </div>
+                        </div>
+                      )
+                    })
+                  }
+                  {
+                    this.state.free[id] ? (
+                      null
+                    ) : (
+                      <div className="row center">
+                        <button onClick={() => {
+                          this.state.discounts[id] ? this.state.discounts[id].push(this.state.discounts[id][this.state.discounts[id].length - 1] + 1) : this.state.discounts[id] = [0];
+                          this.forceUpdate();
+                        }}>Add A Discount</button>
+                      </div>
+                    )
+                  }
                 </div>
               )
             })
