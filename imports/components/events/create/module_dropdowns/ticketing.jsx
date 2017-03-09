5a8cb0b599d3fe5de062352f2c52cc4310dfca3f
Steven Chen <steven@brachyon.com>
2017-03-09 13:16:49 -0800

    Create refactor complete for ticketing, allows ability to add descriptions to tickets and discounts, and separates discounts and fees in the ticket module

--- imports/components/events/create/module_dropdowns/ticketing.jsx
+++ imports/components/events/create/module_dropdowns/ticketing.jsx
@@ -6,146 +6,196 @@ export default class TicketingPanel extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      paymentType: "onsite",
+      paymentType: "cash",
+      fees: {
+        venue: {
+          price: (0).toFixed(2)
+        }
+      },
       discounts: {},
-      free: {}
+      discountCount: 0,
+      active: 0
     }
   }
 
+  componentWillReceiveProps(next) {
+    var obj = {
+      venue: this.state.fees.venue
+    };
+    next.brackets.forEach(k => {
+      obj[k] = this.state.fees[k] || { price: (0).toFixed(2) };
+    });
+    this.state.fees = obj;
+  }
+
   value() {
-    var obj = {};
-    obj["venue"] = parseInt(parseFloat(this.refs.venue.value) * 100);
-    this.props.brackets.forEach((b, i) => {
-      if(this.state.free[i]) {
-        obj[i] = {
-          price: 0
+
+    var feeObj = {};
+    Object.keys(this.state.fees).forEach(k => {
+      feeObj[k] = {
+        price: parseInt(parseFloat(this.state.fees[k].price) * 100),
+        description: this.state.fees[k].description
+      };
+    })
+    var obj = {
+      fees: feeObj,
+      discounts: Object.keys(this.state.discounts).map(k => {
+        if(this.state.discounts[k].name == "") {
+          toastr.error("Your discount needs a name!");
+          throw new Error("Discounts must be named.");
         }
+        if(this.state.discounts[k].price == 0) {
+          toastr.error("Discounts can't be zero!");
+          throw new Error("Can't have a free discount!");
+        }
+        return this.state.discounts[k];
+      })
+    };
+    obj.paymentType = this.state.paymentType;
+    return obj;
+  }
+
+  feePanel() {
+    return Object.keys(this.state.fees).sort((a, b) => {
+      if(isNaN(a)) {
+        if(isNaN(b)) {
+          return 0;
+        }
+        return -1;
       }
-      else {
-        obj[i] = {
-          price: parseInt(parseFloat(this.refs[i].value) * 100),
-          discounts: (this.state.discounts[i] || []).map(j => {
-            return {
-              name: this.refs[i + "_discountName_" + j].value,
-              price: parseInt(parseFloat(this.refs[i + "_discountPrice_" + j].value) * 100)
-            }
+      else if(isNaN(b)) {
+        return 1;
+      }
+      const diff = parseInt(a) - parseInt(b);
+      return diff / Math.abs(diff);
+    }).map(key => {
+      return (
+        <div className="row" style={{marginBottom: 10}} key={key}>
+          <div className="col-1">
+            <span>{ isNaN(key) ? key[0].toUpperCase() + key.slice(1) : "Entry to Bracket " + (parseInt(key) + 1) }</span>
+          </div>
+          <div className="col col-2">
+            <div className="row x-center">
+              $
+              <input type="number" style={{margin: 0, marginLeft: 10, marginRight: 10}} value={this.state.fees[key].price} onChange={(e) => {
+                this.state.fees[key].price = parseFloat(e.target.value);
+                this.forceUpdate();
+              }} onBlur={(e) => {
+                this.state.fees[key].price = parseFloat(e.target.value).toFixed(2);
+                this.forceUpdate();
+              }} />
+              <div className="row x-center">
+                <input type="checkbox" checked={this.state.fees[key].price == 0} onClick={() => {
+                  this.state.fees[key].price = (0).toFixed(2);
+                  this.forceUpdate();
+                }} style={{margin: 0, marginRight: 10}} />
+                <span>Is this fee free?</span>
+              </div>
+            </div>
+            <textarea style={{marginLeft: 18}} placeholder="Description (Optional)" onChange={(e) => {
+              this.state.fees[key].description = e.target.value;
+            }} value={this.state.fees[key].description}></textarea>
+          </div>
+        </div>
+      )
+    })
+  }
+
+  discountPanel() {
+    return (
+      <div>
+        {
+          Object.keys(this.state.discounts).map(key => {
+            var d = this.state.discounts[key];
+            return (
+              <div className="row" style={{marginBottom: 10, justifyContent: "flex-start"}} key={key}>
+                <div className="col col-1">
+                  <div className="row x-center">
+                    <input type="text" style={{margin: 0}} defaultValue={d.name} onChange={(e) => {
+                      d.name = e.target.value
+                    }} />
+                    <FontAwesome name="times" size="2x" style={{marginLeft: 10}} onClick={() => {
+                      delete this.state.discounts[key];
+                      this.forceUpdate();
+                    }} />
+                  </div>
+                </div>
+                <div className="col col-2">
+                  <div className="row x-center">
+                    $
+                    <input type="number" style={{margin: 0, marginLeft: 10}} defaultValue={(d.price / 100).toFixed(2)} onChange={(e) => {
+                      d.price = parseInt(parseFloat(e.target.value) * 100)
+                    }} onBlur={(e) => {
+                      e.target.value = parseFloat(e.target.value).toFixed(2)
+                    }} />
+                  </div>
+                  <textarea style={{marginLeft: 18}} placeholder="Description (Optional)" onChange={(e) => {
+                    d.description = e.target.value;
+                  }} value={d.description}></textarea>
+                </div>
+              </div>
+            )
           })
         }
-      }
-    });
-    return obj;
+        <button onClick={() => {
+          this.state.discounts[this.state.discountCount++] = {
+            name: "",
+            price: 0
+          };
+          this.forceUpdate()
+        }}>Add Discount</button>
+      </div>
+    );
+  }
+
+  currentPanel() {
+    switch(this.state.active) {
+      case 0:
+        return this.feePanel()
+      case 1:
+        return this.discountPanel()
+    }
   }
 
   render() {
     if(this.props.status) {
       return (
         <div className="col">
-          <div className="row x-center" style={{marginBottom: 10}}>
-            <div className="col-1">
-              Venue Fee
-            </div>
-            <div className="col-1">
-              $<input ref="venue" type="number" style={{margin: 0, marginLeft: 10}} defaultValue={0} />
-            </div>
+          <div className="row" style={{marginBottom: 20}}>
+            {
+              ["Fees", "Discounts"].map((a, b) => {
+                return (
+                  <div style={{borderBottom: this.state.active == b ? "solid 2px #FF6000" : "none", padding: 5, cursor: "pointer"}} onClick={() => { this.setState({
+                    active: b
+                  }) }}>
+                    { a }
+                  </div>
+                )
+              })
+            }
           </div>
           {
-            this.props.brackets.map((id, i) => {
-              return (
-                <div className="col" style={{marginBottom: 10, padding: 10, border: "solid 2px #666"}} key={id}>
-                  <div className="row x-center" style={{marginBottom: 10}}>
-                    <div className="col-1">
-                      Bracket {i + 1}
-                    </div>
-                    <div className="row col-1 x-center">
-                      {
-                        this.state.free[id] ? (
-                          <div style={{margin: "0 10px"}}>Free</div>
-                        ) : (
-                          <div>
-                            $<input ref={id} type="number" style={{margin: "0 10px"}} defaultValue={0} />
-                          </div>
-                        )
-                      }
-                      <div className="row x-center">
-                        <input type="checkbox" style={{margin: 0, marginRight: 10}} checked={this.state.free[id] == true} onClick={() => {
-                          if(this.state.free[id]) {
-                            delete this.state.free[id];
-                          }
-                          else {
-                            this.state.free[id] = true;
-                            delete this.state.discounts[id];
-                          }
-                          this.forceUpdate();
-                        }} />
-                        <span>
-                          Is this bracket free?
-                        </span>
-                      </div>
-                    </div>
-                  </div>
-                  {
-                    this.state.free[id] ? (
-                      null
-                    ) : (
-                      <hr className="user-divider" />
-                    )
-                  }
-                  {
-                    this.state.discounts[id] ? (
-                      <div className="row center">
-                        <label>Discounts</label>
-                      </div>
-                    ) : (
-                      null
-                    )
-                  }
-                  {
-                    (this.state.discounts[id] || []).map((val, i) => {
-                      return (
-                        <div className="row x-center" style={{marginBottom: 10}} key={id + "_" + val}>
-                          <div className="col-1">
-                            <input type="text" ref={id + "_discountName_" + i} style={{margin: 0, marginRight: 10}} defaultValue={""} placeholder="Discount Name" />
-                          </div>
-                          <div className="row x-center col-1">
-                            $<input type="number" ref={id + "_discountPrice_" + i} style={{margin: "0 10px"}} defaultValue={0} />
-                            <FontAwesome size="2x" name="times" onClick={() => {
-                              this.state.discounts[id].splice(i, 1);
-                              this.forceUpdate();
-                            }} />
-                          </div>
-                        </div>
-                      )
-                    })
-                  }
-                  {
-                    this.state.free[id] ? (
-                      null
-                    ) : (
-                      <div className="row center">
-                        <button onClick={() => {
-                          this.state.discounts[id] ? this.state.discounts[id].push(this.state.discounts[id][this.state.discounts[id].length - 1] + 1) : this.state.discounts[id] = [0];
-                          this.forceUpdate();
-                        }}>Add A Discount</button>
-                      </div>
-                    )
-                  }
-                </div>
-              )
-            })
+            this.currentPanel()
           }
           <span>Payment Options</span>
           <form ref="paymentOption">
             <div className="row">
-              <input type="radio" name="type" checked={this.state.paymentType == "onsite"} onClick={() => { this.setState({
-                paymentType: "onsite"
+              <input type="radio" name="type" checked={this.state.paymentType == "cash"} onClick={() => { this.setState({
+                paymentType: "cash"
               })}} />
-              <span>Cash Only</span>
+              <span style={{marginLeft: 5}}>Cash Only</span>
+            </div>
+            <div className="row">
+              <input type="radio" name="type" checked={this.state.paymentType == "credit"} onClick={() => { this.setState({
+                paymentType: "credit"
+              })}} />
+              <span style={{marginLeft: 5}}>Credit Only</span>
             </div>
             <div className="row">
               <input name="type" type="radio" checked={this.state.paymentType == "both"} onClick={() => { this.setState({
                 paymentType: "both"
               })}} />
-              <span>Cash And Credit</span>
+              <span style={{marginLeft: 5}}>Cash And Credit</span>
             </div>
           </form>
         </div>
