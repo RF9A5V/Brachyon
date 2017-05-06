1e82386c4f63bf755921dd31ec24c8322c61cba4
Steven Chen <steven@brachyon.com>
2017-05-05 23:23:31 -0700

    I'm dumb and forgot to add the file name field for the event admin

--- imports/components/events/admin.jsx
+++ imports/components/events/admin.jsx
@@ -324,6 +324,7 @@ class EventAdminPage extends Component {
       Banners.insert({
         file: imgTemp.image,
         meta: imgTemp.meta,
+        fileName: event.details._id + "." + imgTemp.type,
         onUploaded: (err, data) => {
           if(err) {
             return toastr.error(err.reason, "Error!");
