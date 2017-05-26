55b50fd3fe4f64e6d876224f3dd370b6f6dbca4f
Steven Chen <steven@brachyon.com>
2017-05-26 15:10:53 -0700

    Added mobile design

--- imports/components/events/preview/sponsor_modal.jsx
+++ imports/components/events/preview/sponsor_modal.jsx
@@ -4,7 +4,9 @@ import FontAwesome from "react-fontawesome";
 
 import { connectFB, connectTwitter } from "/imports/decorators/social_media.js";
 
-export default class SponsorModal extends Component {
+import ResponsiveComponent from "/imports/components/public/responsive_component.jsx";
+
+export default class SponsorModal extends ResponsiveComponent {
 
   constructor(props) {
     super(props);
@@ -12,7 +14,7 @@ export default class SponsorModal extends Component {
     this.link = window.location.origin + "/event/" + this.event.slug;
     this.state = {
       open: false,
-      tweet: `Check out ${this.event.details.name} @ ${this.link}`
+      tweet: `Check out ${this.event.details.name}!`
     }
   }
 
@@ -164,33 +166,39 @@ export default class SponsorModal extends Component {
     )
   }
 
-  renderMain() {
+  renderMain(opts) {
     const user = Meteor.user();
+    const fontStyle = {
+      fontSize: opts.fontSize
+    };
     return (
-      <div className="col">
+      <div className="col" style={{padding: opts.mobile ? 40: 0}}>
         <div className="row" style={{justifyContent: "flex-end", marginBottom: 10}}>
-          <FontAwesome name="times" size="2x" onClick={this.close.bind(this)} />
+          <FontAwesome name="times" style={{fontSize: opts.iconSize}} onClick={this.close.bind(this)} />
         </div>
         <div className="col col-1">
-          <label className="input-label">Tweet Content { this.state.tweet.length } / 140</label>
-          <textarea ref="twitterInput" style={{margin: 0, marginBottom: 10}} placeholder="Share this on Twitter" value={this.state.tweet} onChange={this.onTweetChange.bind(this)}>
-            Check out {this.event.details.name} @ { this.link }
+          <p style={{marginBottom: 0, fontWeight: "bold", textAlign: "center", fontSize: opts.fontSize}}>For every share, we'll put a dollar into the prize pool!</p>
+          <hr className="user-divider" />
+          <label style={fontStyle} className="input-label">Tweet Content { this.state.tweet.length } / { 140 - this.link.length }</label>
+          <textarea ref="twitterInput" style={{margin: 0, fontSize: opts.fontSize}} placeholder="Share this on Twitter" value={this.state.tweet} onChange={this.onTweetChange.bind(this)}>
+            Check out {this.event.details.name}!
           </textarea>
+          <label className="input-label" style={{marginBottom: 10, fontSize: opts.fontSize}}>{ this.link }</label>
           <div className="row center x-center">
-            <button className="col-1" onClick={this.shareToTwitter.bind(this)}>
+            <button className={`col-1 ${opts.buttonClass}`} onClick={this.shareToTwitter.bind(this)}>
               <FontAwesome name="twitter" style={{marginRight: 10}} />
               <span>Share on Twitter</span>
             </button>
           </div>
           <hr className="user-divider" />
-          <label className="input-label">Facebook Content</label>
-          <textarea ref="facebookInput" style={{margin: 0, marginBottom: 10}} placeholder="Share this on Facebook"></textarea>
+          <label style={fontStyle} className="input-label">Facebook Content</label>
+          <textarea ref="facebookInput" style={{margin: 0, fontSize: opts.fontSize, marginBottom: 10}} placeholder="Share this on Facebook"></textarea>
           <div className="row center x-center">
-            <button className="col-1" style={{marginRight: 10}} onClick={this.addLinkToFacebookPost.bind(this)}>
+            <button className={`col-1 ${opts.buttonClass}`} style={{marginRight: 10}} onClick={this.addLinkToFacebookPost.bind(this)}>
               <FontAwesome name="link" style={{marginRight: 10}} />
               <span>Add Link</span>
             </button>
-            <button className="col-1" onClick={this.shareToFacebook.bind(this)}>
+            <button className={`col-1 ${opts.buttonClass}`} onClick={this.shareToFacebook.bind(this)}>
               <FontAwesome name="facebook" style={{marginRight: 10}} />
               <span>Share on Facebook</span>
             </button>
@@ -202,13 +210,13 @@ export default class SponsorModal extends Component {
               [
                 <hr className="user-divider" />,
                 <div className="col" style={{marginBottom: 10}}>
-                  <label className="input-label">Phone Number</label>
+                  <label style={fontStyle} className="input-label">Phone Number</label>
                   <input type="text" style={{margin: 0}} placeholder="(555) 555-5555" ref="phoneNumber" defaultValue={user.profile.phoneNumber ? user.profile.phoneNumber.slice(2) : ""} />
                 </div>,
                 <div className="row center x-center">
                   {
                     user.profile.phoneNumber ? (
-                      <button className="col-1" style={{marginRight: 10}} onClick={() => {
+                      <button className={`col-1 ${opts.buttonClass}`} style={{marginRight: 10}} onClick={() => {
                         this.setState({
                           showMobileVerification: true
                         })
@@ -217,9 +225,9 @@ export default class SponsorModal extends Component {
                       null
                     )
                   }
-                  <button className="col-1" onClick={this.saveNumber.bind(this)}>
+                  <button className={`col-1 ${opts.buttonClass}`} onClick={this.saveNumber.bind(this)}>
                     <FontAwesome name="mobile" style={{marginRight: 10}} />
-                    <span>{user.profile.phoneNumber ? "Update Phone Number" : "Save Phone Number"}</span>
+                    <span style={fontStyle}>{user.profile.phoneNumber ? "Update Phone Number" : "Save Phone Number"}</span>
                   </button>
                 </div>
               ]
@@ -230,18 +238,42 @@ export default class SponsorModal extends Component {
     )
   }
 
-  render() {
+  renderBase(opts) {
     const user = Meteor.user();
     return (
-      <Modal isOpen={this.state.open} onRequestClose={this.close.bind(this)}>
+      <Modal isOpen={this.state.open} onRequestClose={this.close.bind(this)} className={opts.modalClass} overlayClassName={opts.overlayClass}>
         {
           this.state.showMobileVerification ? (
-            this.renderValidationForMobile()
+            this.renderValidationForMobile(opts)
           ) : (
-            this.renderMain()
+            this.renderMain(opts)
           )
         }
       </Modal>
     )
+  }
+
+  renderDesktop() {
+    return this.renderBase({
+      modalClass: "",
+      overlayClass: "",
+      inputClass: "",
+      buttonClass: "",
+      fontSize: "1em",
+      mobile: false,
+      iconSize: "2em"
+    })
+  }
+
+  renderMobile() {
+    return this.renderBase({
+      modalClass: "overlay-modal",
+      overlayClass: "overlay-only",
+      inputClass: "large-input",
+      buttonClass: "large-button",
+      fontSize: "3em",
+      mobile: true,
+      iconSize: "3.5em"
+    })
   }
 }
